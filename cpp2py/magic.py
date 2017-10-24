cb939d260272a6855adc8dfc116c90ffac495701
Olivier Parcollet <olivier.parcollet@cea.fr>
2017-10-24 17:47:27 +0200

    [magic] Add the -C option to the magic cell
    - C pytriqs, e.g. uses pytriqs

--- cpp2py/magic.py
+++ cpp2py/magic.py
@@ -32,6 +32,7 @@ class Cpp2pyMagics(Magics):
     @magic_arguments.magic_arguments()
     @magic_arguments.argument( "-v", "--verbosity", type=int, help="increase output verbosity")
     @magic_arguments.argument( '-o', "--only", action='append', default=[], help="""Which object to wrap""")
+    @magic_arguments.argument( '-C', "--converters", action='append', default=[], help="""Modules""")
     @cell_magic
     def cpp2py(self, line, cell=None):
         """Compile and import everything from a Cpp2py code cell.
@@ -64,7 +65,7 @@ class Cpp2pyMagics(Magics):
 
         args = magic_arguments.parse_argstring(self.cpp2py, line)
         code = cell if cell.endswith('\n') else cell + '\n'
-        module = compile(code, args.verbosity, args.only)
+        module = compile(code, verbosity = args.verbosity, only = args.only, modules = args.converters)
 
         # import all object and function in the main namespace
         imported = []
