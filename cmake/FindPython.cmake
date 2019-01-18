d9cbd6b9fb5ffacbf5363b6927e9c2346eb71d7c
Nils Wentzell <nwentzell@flatironinstitute.org>
2019-01-18 11:36:53 -0500

    [cmake] Make PYTHON_LIB_DEST_ROOT Cache variable, formatting FindPython.cmake

--- cmake/FindPython.cmake
+++ cmake/FindPython.cmake
@@ -24,85 +24,85 @@
 set(PYTHON_MINIMAL_VERSION 2.6.5)
 include(FindPackageHandleStandardArgs)
 
-MESSAGE( STATUS "-------- Python detection -------------")
+message( STATUS "-------- Python detection -------------")
 
 # IF PYTHON_INTERPRETER is not defined, try to find a python
 if(NOT PYTHON_INTERPRETER)
  find_program(PYTHON_INTERPRETER python PATHS $ENV{PATH})
 endif(NOT PYTHON_INTERPRETER)
 if(NOT PYTHON_INTERPRETER)
- MESSAGE(FATAL_ERROR "No python interpreter found")
+ message(FATAL_ERROR "No python interpreter found")
 endif(NOT PYTHON_INTERPRETER)
 
-MESSAGE (STATUS "Python interpreter ${PYTHON_INTERPRETER}")
+message(STATUS "Python interpreter ${PYTHON_INTERPRETER}")
 #
 # The function EXEC_PYTHON_SCRIPT executes the_script in  python interpreter
 # and set the variable of output_var_name in the calling scope
 #
-FUNCTION ( EXEC_PYTHON_SCRIPT the_script output_var_name)
+FUNCTION( EXEC_PYTHON_SCRIPT the_script output_var_name)
  EXECUTE_PROCESS(COMMAND ${PYTHON_INTERPRETER} -c "${the_script}"
   OUTPUT_VARIABLE res RESULT_VARIABLE returncode OUTPUT_STRIP_TRAILING_WHITESPACE)
- if (NOT returncode EQUAL 0)
-  MESSAGE(FATAL_ERROR "The script : ${the_script} \n did not run properly in the Python interpreter. Check your python installation.")
- endif (NOT returncode EQUAL 0)
+ if(NOT returncode EQUAL 0)
+  message(FATAL_ERROR "The script : ${the_script} \n did not run properly in the Python interpreter. Check your python installation.")
+ endif(NOT returncode EQUAL 0)
  SET( ${output_var_name} ${res} PARENT_SCOPE)
-ENDFUNCTION (EXEC_PYTHON_SCRIPT)
+ENDFUNCTION(EXEC_PYTHON_SCRIPT)
 
  #
  # Check the interpreter and its version
  #
- EXEC_PYTHON_SCRIPT ("import sys, string; print sys.version.split()[0]" PYTHON_VERSION)
+ EXEC_PYTHON_SCRIPT("import sys, string; print sys.version.split()[0]" PYTHON_VERSION)
  if(${PYTHON_VERSION} VERSION_LESS ${PYTHON_MINIMAL_VERSION})
-  MESSAGE(FATAL_ERROR "Python intepreter version is ${PYTHON_VERSION} . It should be >= ${PYTHON_MINIMAL_VERSION}")
- endif ()
- EXEC_PYTHON_SCRIPT ("import sys; print sys.version_info[0]" PYTHON_VERSION_MAJOR)
- EXEC_PYTHON_SCRIPT ("import sys; print sys.version_info[1]" PYTHON_VERSION_MINOR)
+  message(FATAL_ERROR "Python intepreter version is ${PYTHON_VERSION} . It should be >= ${PYTHON_MINIMAL_VERSION}")
+ endif()
+ EXEC_PYTHON_SCRIPT("import sys; print sys.version_info[0]" PYTHON_VERSION_MAJOR)
+ EXEC_PYTHON_SCRIPT("import sys; print sys.version_info[1]" PYTHON_VERSION_MINOR)
  
- EXEC_PYTHON_SCRIPT ("import mako.template" nulle) # check that Mako is there...
- EXEC_PYTHON_SCRIPT ("import distutils " nulle) # check that distutils is there...
- EXEC_PYTHON_SCRIPT ("import numpy" nulle) # check that numpy is there...
- EXEC_PYTHON_SCRIPT ("import h5py" nulle) # check that h5py is there...
- EXEC_PYTHON_SCRIPT ("import scipy" nulle) # check that scipy is there...
+ EXEC_PYTHON_SCRIPT("import mako.template" nulle) # check that Mako is there...
+ EXEC_PYTHON_SCRIPT("import distutils " nulle) # check that distutils is there...
+ EXEC_PYTHON_SCRIPT("import numpy" nulle) # check that numpy is there...
+ EXEC_PYTHON_SCRIPT("import h5py" nulle) # check that h5py is there...
+ EXEC_PYTHON_SCRIPT("import scipy" nulle) # check that scipy is there...
   
- #EXEC_PYTHON_SCRIPT ("import mpi4py" nulle) # check that mpi4py is there...
+ #EXEC_PYTHON_SCRIPT("import mpi4py" nulle) # check that mpi4py is there...
  
  # Adjust this
  if(Build_Documentation)
-  EXEC_PYTHON_SCRIPT ("import clang.cindex" nulle) # check that libclang is there...
+  EXEC_PYTHON_SCRIPT("import clang.cindex" nulle) # check that libclang is there...
  endif(Build_Documentation)
- MESSAGE(STATUS "Python interpreter and modules are ok : version ${PYTHON_VERSION}" )
+ message(STATUS "Python interpreter and modules are ok : version ${PYTHON_VERSION}")
  
  #
  # Check for Python include path
  #
- EXEC_PYTHON_SCRIPT ("import distutils ; from distutils.sysconfig import * ; print distutils.sysconfig.get_python_inc()"  PYTHON_INCLUDE_DIRS )
- message(STATUS "PYTHON_INCLUDE_DIRS =  ${PYTHON_INCLUDE_DIRS}" )
+ EXEC_PYTHON_SCRIPT("import distutils ; from distutils.sysconfig import * ; print distutils.sysconfig.get_python_inc()"  PYTHON_INCLUDE_DIRS)
+ message(STATUS "PYTHON_INCLUDE_DIRS =  ${PYTHON_INCLUDE_DIRS}")
  mark_as_advanced(PYTHON_INCLUDE_DIRS)
- FIND_PATH(TEST_PYTHON_INCLUDE patchlevel.h PATHS ${PYTHON_INCLUDE_DIRS} NO_DEFAULT_PATH)
+ find_path(TEST_PYTHON_INCLUDE patchlevel.h PATHS ${PYTHON_INCLUDE_DIRS} NO_DEFAULT_PATH)
  if(NOT TEST_PYTHON_INCLUDE)
-  message (ERROR "The Python herader files have not been found. Please check that you installed the Python headers and not only the interpreter.")
+  message(ERROR "The Python herader files have not been found. Please check that you installed the Python headers and not only the interpreter.")
  endif(NOT TEST_PYTHON_INCLUDE)
  mark_as_advanced(TEST_PYTHON_INCLUDE)
  
  #
  # HDF5 version used by h5py
  #
- EXEC_PYTHON_SCRIPT ("import h5py;print h5py.version.hdf5_version" PYTHON_H5PY_HDF5VERSION)
+ EXEC_PYTHON_SCRIPT("import h5py;print h5py.version.hdf5_version" PYTHON_H5PY_HDF5VERSION)
  set(PYTHON_H5PY_HDF5VERSION ${PYTHON_H5PY_HDF5VERSION} CACHE STRING "the hdf5 version that h5py was compiled against" FORCE)
- MESSAGE(STATUS "PYTHON_H5PY_HDF5VERSION = ${PYTHON_H5PY_HDF5VERSION}" )
+ message(STATUS "PYTHON_H5PY_HDF5VERSION = ${PYTHON_H5PY_HDF5VERSION}")
  
  #
  # include files for numpy
  #
- EXEC_PYTHON_SCRIPT ("import numpy;print numpy.get_include()" PYTHON_NUMPY_INCLUDE_DIR)
- MESSAGE(STATUS "PYTHON_NUMPY_INCLUDE_DIR = ${PYTHON_NUMPY_INCLUDE_DIR}" )
+ EXEC_PYTHON_SCRIPT("import numpy;print numpy.get_include()" PYTHON_NUMPY_INCLUDE_DIR)
+ message(STATUS "PYTHON_NUMPY_INCLUDE_DIR = ${PYTHON_NUMPY_INCLUDE_DIR}")
  mark_as_advanced(PYTHON_NUMPY_INCLUDE_DIR)
  
  #
  # include files for numpy
  #
- EXEC_PYTHON_SCRIPT ("import numpy;print numpy.version.version" PYTHON_NUMPY_VERSION)
- MESSAGE(STATUS "PYTHON_NUMPY_VERSION = ${PYTHON_NUMPY_VERSION}" )
+ EXEC_PYTHON_SCRIPT("import numpy;print numpy.version.version" PYTHON_NUMPY_VERSION)
+ message(STATUS "PYTHON_NUMPY_VERSION = ${PYTHON_NUMPY_VERSION}")
  mark_as_advanced(PYTHON_NUMPY_VERSION)
  
  # The C API of numpy has changed with 1.7.0, the macro is a version switch in a few files of the libs.
@@ -113,31 +113,31 @@ ENDFUNCTION (EXEC_PYTHON_SCRIPT)
  #
  # Check for site packages
  #
- EXEC_PYTHON_SCRIPT ("from distutils.sysconfig import * ;print get_python_lib(0,0)"
+ EXEC_PYTHON_SCRIPT("from distutils.sysconfig import * ;print get_python_lib(0,0)"
   PYTHON_SITE_PKG)
- MESSAGE(STATUS "PYTHON_SITE_PKG = ${PYTHON_SITE_PKG}" )
+ message(STATUS "PYTHON_SITE_PKG = ${PYTHON_SITE_PKG}")
  mark_as_advanced(PYTHON_SITE_PKG)
  
  #
  # Check for Python library path
  #
- EXEC_PYTHON_SCRIPT ("import string; from distutils.sysconfig import *; print '%s' % get_python_lib(0,1)" PYTHON_LIBRARY_BASE_PATH)
- EXEC_PYTHON_SCRIPT ("import string; from distutils.sysconfig import *; print 'libpython%s' % string.join(get_config_vars('VERSION'))" PYTHON_LIBRARY_BASE_FILE)
- set( PYTHON_LIBRARY_SEARCH_PATHS "${PYTHON_LIBRARY_BASE_PATH}/.." "${PYTHON_LIBRARY_BASE_PATH}/../x86_64-linux-gnu" "${PYTHON_LIBRARY_BASE_PATH}/../i386-linux-gnu" )
+ EXEC_PYTHON_SCRIPT("import string; from distutils.sysconfig import *; print '%s' % get_python_lib(0,1)" PYTHON_LIBRARY_BASE_PATH)
+ EXEC_PYTHON_SCRIPT("import string; from distutils.sysconfig import *; print 'libpython%s' % string.join(get_config_vars('VERSION'))" PYTHON_LIBRARY_BASE_FILE)
+ set( PYTHON_LIBRARY_SEARCH_PATHS "${PYTHON_LIBRARY_BASE_PATH}/.." "${PYTHON_LIBRARY_BASE_PATH}/../x86_64-linux-gnu" "${PYTHON_LIBRARY_BASE_PATH}/../i386-linux-gnu")
  if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  FIND_FILE(PYTHON_LIBRARY NAMES "${PYTHON_LIBRARY_BASE_FILE}.dylib" PATHS ${PYTHON_LIBRARY_SEARCH_PATHS})
+  find_file(PYTHON_LIBRARY NAMES "${PYTHON_LIBRARY_BASE_FILE}.dylib" PATHS ${PYTHON_LIBRARY_SEARCH_PATHS})
  else()
-  FIND_FILE(PYTHON_LIBRARY NAMES "${PYTHON_LIBRARY_BASE_FILE}.so" PATHS ${PYTHON_LIBRARY_SEARCH_PATHS})
+  find_file(PYTHON_LIBRARY NAMES "${PYTHON_LIBRARY_BASE_FILE}.so" PATHS ${PYTHON_LIBRARY_SEARCH_PATHS})
  endif()
- MESSAGE(STATUS "PYTHON_LIBRARY = ${PYTHON_LIBRARY}" )
+ message(STATUS "PYTHON_LIBRARY = ${PYTHON_LIBRARY}")
  mark_as_advanced(PYTHON_LIBRARY)
 
  #
  # libraries which must be linked in when embedding
  #
- EXEC_PYTHON_SCRIPT ("from distutils.sysconfig import * ;print (str(get_config_var('LOCALMODLIBS')) + ' ' + str(get_config_var('LIBS'))).strip()"
+ EXEC_PYTHON_SCRIPT("from distutils.sysconfig import * ;print (str(get_config_var('LOCALMODLIBS')) + ' ' + str(get_config_var('LIBS'))).strip()"
   PYTHON_EXTRA_LIBS)
- MESSAGE(STATUS "PYTHON_EXTRA_LIBS =${PYTHON_EXTRA_LIBS}" )
+ message(STATUS "PYTHON_EXTRA_LIBS =${PYTHON_EXTRA_LIBS}")
  mark_as_advanced(PYTHON_EXTRA_LIBS)
 
  # FIXME : No clue why this is here ?? Why separate_arguments ?
@@ -146,7 +146,7 @@ ENDFUNCTION (EXEC_PYTHON_SCRIPT)
   separate_arguments(PYTHON_EXTRA_LIBS)
  endif()
 
- set (PYTHONLIBS_FOUND TRUE) 
+ set(PYTHONLIBS_FOUND TRUE)
 
  # Define Imported target python_and_numpy
  add_library(python_and_numpy SHARED IMPORTED GLOBAL)
@@ -160,6 +160,6 @@ ENDFUNCTION (EXEC_PYTHON_SCRIPT)
  else()
    set(PYTHON_LIB_DEST_DIR site-packages)
  endif()
- set(PYTHON_LIB_DEST_ROOT lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/${PYTHON_LIB_DEST_DIR})
+ set(PYTHON_LIB_DEST_ROOT lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/${PYTHON_LIB_DEST_DIR} CACHE PATH "Install directory for Python Modules relative to CMAKE_INSTALL_PREFIX")
  message(STATUS "Python modules will be installed in ${CMAKE_INSTALL_PREFIX}/${PYTHON_LIB_DEST_ROOT}")
 
