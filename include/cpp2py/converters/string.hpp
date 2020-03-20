0363eac3ec213f9566027d23a9e941903085fe95
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-03-20 17:46:31 -0400

    Add py_converter for char to cpp2py/converters/string.hpp

--- include/cpp2py/converters/string.hpp
+++ include/cpp2py/converters/string.hpp
@@ -16,6 +16,19 @@ namespace cpp2py {
     }
   };
 
+  template <> struct py_converter<char> {
+
+    static PyObject *c2py(char c) { return PyUnicode_FromString(&c); }
+
+    static char py2c(PyObject *ob) { return PyUnicode_AsUTF8(ob)[0]; }
+
+    static bool is_convertible(PyObject *ob, bool raise_exception) {
+      if (PyUnicode_Check(ob) and PyUnicode_GET_LENGTH(ob) == 1) return true;
+      if (raise_exception) { PyErr_SetString(PyExc_TypeError, "Cannot convert to char"); }
+      return false;
+    }
+  };
+
   template <> struct py_converter<const char *> {
 
     static PyObject *c2py(const char *x) { return PyUnicode_FromString(x); }
@@ -23,7 +36,7 @@ namespace cpp2py {
     static const char *py2c(PyObject *ob) { return PyUnicode_AsUTF8(ob); }
 
     static bool is_convertible(PyObject *ob, bool raise_exception) {
-      if (PyUnicode_Check(ob) or PyUnicode_Check(ob)) return true;
+      if (PyUnicode_Check(ob)) return true;
       if (raise_exception) { PyErr_SetString(PyExc_TypeError, "Cannot convert to string"); }
       return false;
     }
