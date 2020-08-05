cd34df2d7766d9b7896e43653589a6d719e896a8
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-08-05 16:13:53 -0400

    Fix complex py2c conversion to allow for numpy complex types

--- c++/cpp2py/converters/complex.hpp
+++ c++/cpp2py/converters/complex.hpp
@@ -1,5 +1,7 @@
 #pragma once
-#include <Python.h>
+#include "./../pyref.hpp"
+
+#include <numpy/arrayobject.h>
 
 namespace cpp2py {
 
@@ -8,6 +10,18 @@ namespace cpp2py {
   template <> struct py_converter<std::complex<double>> {
     static PyObject *c2py(std::complex<double> x) { return PyComplex_FromDoubles(x.real(), x.imag()); }
     static std::complex<double> py2c(PyObject *ob) {
+
+      if (PyArray_CheckScalar(ob)) {
+        // Convert NPY Scalar Type to Builtin Type
+        pyref py_builtin = PyObject_CallMethod(ob, "item", NULL);
+        if (PyComplex_Check(py_builtin)) {
+          auto r = PyComplex_AsCComplex(py_builtin);
+          return {r.real, r.imag};
+        } else {
+          return PyFloat_AsDouble(py_builtin);
+        }
+      }
+
       if (PyComplex_Check(ob)) {
         auto r = PyComplex_AsCComplex(ob);
         return {r.real, r.imag};
@@ -16,6 +30,10 @@ namespace cpp2py {
     }
     static bool is_convertible(PyObject *ob, bool raise_exception) {
       if (PyComplex_Check(ob) || PyFloat_Check(ob) || PyLong_Check(ob)) return true;
+      if (PyArray_CheckScalar(ob)) {
+        pyref py_arr = PyArray_FromScalar(ob, NULL);
+        if (PyArray_ISINTEGER((PyObject*)py_arr) or PyArray_ISFLOAT((PyObject*)py_arr) or PyArray_ISCOMPLEX((PyObject*)py_arr)) return true;
+      }
       if (raise_exception) { PyErr_SetString(PyExc_TypeError, "Cannot convert to complex"); }
       return false;
     }
