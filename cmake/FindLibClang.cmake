7e9ab43b5d2085cd88210e31dd09d931c9386b29
Nils Wentzell <nwentzell@flatironinstitute.org>
2021-01-20 10:20:46 -0500

    [cmake] Use find_XXX(.. HINTS ..) over find_XXX(.. PATHS ..) to not prioritize system dirs

--- cmake/FindLibClang.cmake
+++ cmake/FindLibClang.cmake
@@ -12,19 +12,23 @@ include(FindPackageHandleStandardArgs)
 
 if( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 
-  set(TRIAL_LIBRARY_PATHS
-    ENV LIBRARY_PATH
-    /usr/local/opt/llvm/lib/
-    /usr/local/Cellar/llvm/9.0.0/lib/
-    /usr/local/Cellar/llvm/8.0.1/lib/
-    /usr/local/Cellar/llvm/8.0.0/lib/
-    /usr/local/Cellar/llvm/7.0.0/lib/
-    /usr/local/Cellar/llvm/6.0.0/lib/
-    /usr/local/Cellar/llvm/5.0.1/lib/
-    /usr/local/lib/
-    /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/
-  )
-  find_library(LIBCLANG_LOCATION NAMES libclang.dylib PATHS ${TRIAL_LIBRARY_PATHS} DOC "Location of the libclang library")
+  find_library(LIBCLANG_LOCATION
+    NAMES libclang.dylib
+    HINTS
+      ENV LIBRARY_PATH
+      /usr/local/opt/llvm/lib/
+      /usr/local/Cellar/llvm/12.0.0/lib/
+      /usr/local/Cellar/llvm/11.0.0/lib/
+      /usr/local/Cellar/llvm/10.0.0/lib/
+      /usr/local/Cellar/llvm/9.0.0/lib/
+      /usr/local/Cellar/llvm/8.0.1/lib/
+      /usr/local/Cellar/llvm/8.0.0/lib/
+      /usr/local/Cellar/llvm/7.0.0/lib/
+      /usr/local/Cellar/llvm/6.0.0/lib/
+      /usr/local/Cellar/llvm/5.0.1/lib/
+      /usr/local/lib/
+      /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/
+    DOC "Location of the libclang library")
 
   set(LIBCLANG_CXX_FLAGS "" CACHE STRING "Additional flags to be passed to libclang when parsing with clang")
 
@@ -33,21 +37,25 @@ if( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 
 else()
 
-  SET(TRIAL_LIBRARY_PATHS
-    ENV LIBRARY_PATH
-    ENV LD_LIBRARY_PATH
-    /usr/lib
-    /usr/lib/x86_64-linux-gnu
-    /usr/lib/llvm-9/lib
-    /usr/lib/llvm-8/lib
-    /usr/lib/llvm-7/lib
-    /usr/lib/llvm-6.0/lib
-    /usr/lib/llvm-5.0/lib
-    /usr/lib/llvm-4.0/lib
-    /usr/lib64/llvm
-  )
   if(NOT LIBCLANG_LOCATION)
-    find_library(LIBCLANG_LOCATION NAMES libclang.so PATHS ${TRIAL_LIBRARY_PATHS} DOC "Location of the libclang library")
+    find_library(LIBCLANG_LOCATION
+      NAMES libclang.so
+      HINTS
+        ENV LIBRARY_PATH
+        ENV LD_LIBRARY_PATH
+        /usr/lib
+        /usr/lib/x86_64-linux-gnu
+        /usr/lib/llvm-12/lib
+        /usr/lib/llvm-11/lib
+        /usr/lib/llvm-10/lib
+        /usr/lib/llvm-9/lib
+        /usr/lib/llvm-8/lib
+        /usr/lib/llvm-7/lib
+        /usr/lib/llvm-6.0/lib
+        /usr/lib/llvm-5.0/lib
+        /usr/lib/llvm-4.0/lib
+        /usr/lib64/llvm
+      DOC "Location of the libclang library")
   endif()
 
   set(LIBCLANG_CXX_FLAGS "${LIBCLANG_CXX_FLAGS}")
@@ -55,11 +63,22 @@ else()
 
   # Now find the clang compiler ....
   if(NOT CLANG_COMPILER)
-    set(TRIAL_CLANG_PATHS
-      ENV PATH
-      /usr/bin
-    )
-    find_program(CLANG_COMPILER names clang++ clang++-9 clang++-8 clang++-7 clang++-6.0 clang++-5.0 clang++-4.0 PATHS ${TRIAL_CLANG_PATHS} DOC "Clang compiler (for libclang option)")
+    find_program(CLANG_COMPILER
+      NAMES
+        clang++
+        clang++-12
+        clang++-11
+        clang++-10
+        clang++-9
+        clang++-8
+        clang++-7
+        clang++-6.0
+        clang++-5.0
+        clang++-4.0
+      HINTS
+        ENV PATH
+        /usr/bin
+      DOC "Clang compiler (for libclang option)")
   endif()
 
   if(NOT CLANG_COMPILER)
