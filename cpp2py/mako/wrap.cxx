368eec907b083c60312ee446de42c25fb04b915a
Nils Wentzell <nwentzell@flatironinstitute.org>
2022-06-01 15:15:05 -0400

    Do not generate escaped charaters in doc strings

--- cpp2py/mako/wrap.cxx
+++ cpp2py/mako/wrap.cxx
@@ -436,7 +436,7 @@ static PyTypeObject ${c.py_type}Type = {
     0,                         /*tp_setattro*/
     0,                         /*tp_as_buffer*/
     Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
-    "${get_c_str_literal(c.doc.encode('unicode_escape').decode('utf-8'))}", /* tp_doc */
+    "${get_c_str_literal(c.doc)}", /* tp_doc */
     0,		               /* tp_traverse */
     0,		               /* tp_clear */
     ${c.py_type + "_richcompare"},  /* tp_richcompare */
